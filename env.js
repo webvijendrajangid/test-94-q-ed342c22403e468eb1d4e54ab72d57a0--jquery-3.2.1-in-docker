bad9e5cad3588145b0d1bbaf3aecbdb158921a5c
Vijendra Jangid <jangidvijendra3@gmail.com>
2019-12-09 17:44:34 +0530

    Initial commit

---
+++ env.js
@@ -0,0 +1,1 @@
+API_URL='http://restapi:8080'
---
+++ index.js
@@ -0,0 +1,13 @@
+$(function(){
+    
+    // accessing API_URL present in env.js
+    console.log(API_URL);
+    
+    $('#dynamic-content').text("Dynamic content");
+
+    
+    
+
+    // jQuery methods go here...
+  
+});
